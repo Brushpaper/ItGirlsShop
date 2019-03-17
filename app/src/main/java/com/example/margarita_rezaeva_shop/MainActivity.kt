809f88f56769843059da01a4f4840cdf0942d359
Brushpaper <hasko-mob@yandex.ru>
2019-03-18 01:28:34 +0300

    unit

--- app/src/main/java/com/example/margarita_rezaeva_shop/MainActivity.kt
+++ app/src/main/java/com/example/margarita_rezaeva_shop/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.margarita_rezaeva_shop
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat.startActivity
 import android.view.View
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.sdk27.coroutines.onClick
 import org.jetbrains.anko.startActivity
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,5 +22,10 @@ class MainActivity : AppCompatActivity() {
         MainAct_BottomText.onClick(){
             startActivity<CategoryActivity>()
         }
+
+        Picasso.get()
+            .load("https://user-images.githubusercontent.com/48313651/54498159-67bb1c00-4914-11e9-81dd-419fee177e3f.png")
+            .into(pictureView)
     }
+
 }
